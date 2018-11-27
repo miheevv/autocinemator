c1366843799970d09fd25beae82229d67c8bebd6
miheevv <miheevv@gmail.com>
2018-11-27 17:07:19 +0300

    work quality

--- cinematorworker.py
+++ cinematorworker.py
@@ -2,13 +2,26 @@
 import os
 from bs4 import BeautifulSoup
 import requests
+import pymongo
+from pymongo import MongoClient
+from datetime import datetime
+from pprint import pprint
 
 # settings
 login_url = 'http://cinemate.cc/login/'
-main_url = 'http://cinemate.cc/'
+top_url = 'http://cinemate.cc/#top_sites_7'
 headers = {'User-Agent': 'Mozilla/5.0'}
 username = os.environ.get('CINEMATE_USERNAME')
 password = os.environ.get('CINEMATE_PASSWORD')
+# DB
+db_URI = 'mongodb://localhost:27017/'
+db_name = 'autocinemator'
+
+# return mongo db object
+def get_db():
+    client = MongoClient(db_URI)
+    db = client[db_name]
+    return db
 
 client = requests.session()
 r = client.get(login_url, headers=headers)
@@ -22,7 +35,7 @@ if 'cm_token' in cinematecokies:
     print('founded :', csrftoken)
 else:
     # get token from form
-    bsObj = BeautifulSoup(r.content, "html.parser")
+    bsObj = BeautifulSoup(r.content, 'html.parser')
     loginform = bsObj.find(id='login_form')
     csrfmiddlewaretoken = loginform.find('', {'name': 'csrfmiddlewaretoken'})
     csrftoken = csrfmiddlewaretoken.attrs['value']
@@ -34,15 +47,35 @@ params = {'username': username,
 
 print(params)
 r = client.post(login_url, headers=headers, data=params)
-bsObj = BeautifulSoup(r.content, "html.parser")
-print(bsObj.find('', {'class': 'username'}))
+'''
+bsObj = BeautifulSoup(r.content, 'html.parser') 
+print(bsObj.find('',{'class': 'username'}))
 print(client.cookies.get_dict(domain='cinemate.cc'))
-
 # print(r.text)
+'''
 
-r = client.get(main_url, headers=headers)
-bsObj = BeautifulSoup(r.content, "html.parser")
-print(bsObj.find('', {'class': 'username'}))
-
+newfilms = []
+r = client.get(top_url, headers=headers)
+bsObj = BeautifulSoup(r.content, 'html.parser') 
+filmdivs_list = bsObj.find('',{'id': 'top_sites_24'}).find_all('',{'class': 'row delimiter'})
+for filmdiv in filmdivs_list:
+    newfilm = {'name': filmdiv.div.a.attrs['title'],
+                'href': filmdiv.div.a.attrs['href']}
+    newfilms.append(newfilm)
+    # qualitylist = 
+    print ('\nnext: ', filmdiv.div.findNext('div').find_all('a'))
+    # print("next q: ", filmdiv.div.findNext('div'))
+'''    print({'name': filmdiv.div.a.attrs['title'],
+           'href': filmdiv.div.a.attrs['href']})'''
+#                'quality': filmdiv.next_siblings.a.get_text()}
 # print(r.text)
+pprint(newfilms)
 
+db = get_db()
+films = db.films
+'''newfilm = {'name': 'film1', 
+           'added_datetime': datetime.utcnow()}
+films.insert_one(newfilm)
+'''
+for film in films.find():
+    pprint(film)
