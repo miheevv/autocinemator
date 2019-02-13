d533bb3ab2d112d4714b29f29a16924938783333
miheevv <miheevv@gmail.com>
2019-02-13 14:56:41 +0300

    get_extern_torrents

--- cinematorworker.py
+++ cinematorworker.py
@@ -5,7 +5,7 @@ Regulary get new films and torrent links from cinemate.cc.
 Films DB structure:
 films: name, href, quality, categories, date, countries, discription, img
        IMDB_rate, IMDB_count, Kinopoisk_rate, Kinopoisk_count, 
-       added_datetime, torrent_urls, bad_torrent, 
+       added_datetime, torrent_urls, bad_torrent, new (resently added)
        filmfolder (by default - 'films'), to_download, downloaded, to_delete (to delete on server), deleted (don't download in future again)
 
 '''
@@ -70,7 +70,7 @@ def loginin(url, client_session):
 # top_category = 'top_sites_24' or 'top_sites_7'
 def get_new_films(db, url, top_category):
     filmslist =[]
-    films_in_db = db.films
+    # films_in_db = db.films
     current_year = datetime.datetime.utcnow().year
 
     r = client.get(url, headers=headers)
@@ -83,26 +83,26 @@ def get_new_films(db, url, top_category):
         href = top_url + str(filmdiv.div.a.attrs['href'])
         newfilm = {'name': name,
                    'href': href}
-        
+
         # Get list of film quality: Rip, HD, DVD, Экранка
         qualitylist = []
         for item in filmdiv.div.findNext('div').find_all('a'):
             qualitylist += item
 
         # Get highness quality
-        if 'Rip' or 'HD' in qualitylist:
+        if ('Rip' in qualitylist) or ('HD' in qualitylist):
             quality = 'HD' if 'HD' in qualitylist else 'Rip'
             newfilm['quality'] = quality
 
         # Don't add in list if low quality
-        if not ('Rip' or 'HD' in qualitylist): continue
+        if not (('Rip' in qualitylist) or ('HD' in qualitylist)): continue
 
         # Don't add if old year
         filmyear = int(fullname[-5:-1])
         if not filmyear >= (current_year - 1): continue
         
         # Don't add if persist in DB
-        if films_in_db.find_one({'name': name}): continue
+        # if films_in_db.find_one({'name': name}): continue
 
         filmslist.append(newfilm)
     return filmslist
@@ -177,7 +177,7 @@ def filter_films(newfilms, films):
     for item in newfilms:
         removeflag = None
         if ('IMDB_count' not in item) or (item['IMDB_count'] < 5000) or \
-           ('IMDB_rate' not in item) or (item['IMDB_rate'] < 5.5) or \
+           ('IMDB_rate' not in item) or (item['IMDB_rate'] < 6) or \
            ('Kinopoisk_count' not in item) or (item['Kinopoisk_count'] < 1000) or \
            ('Kinopoisk_rate' not in item) or (item['Kinopoisk_rate'] < 5.0):
             removeflag = 1
@@ -190,6 +190,139 @@ def filter_films(newfilms, films):
             notfiltred_films.append(item)
     
     return notfiltred_films
+
+# Return trackers list from film
+def get_trackers(film, type):
+    trackers= []
+    r = client.get(film['href']+'links/#tabs', headers=headers)
+    bsObj = BeautifulSoup(r.content, 'html.parser') 
+    trackers_list = bsObj.find('',{'id': 'site-list'})
+
+    try:
+        trackersHD_list = trackers_list.find('',{'id': type}).parent
+    except:
+        return None
+
+    # Get lang type
+    for trackersdiv in trackersHD_list.find_all('',{'class': 'row delimiter'}):
+        langs = []
+        langs_type ={
+            'Профессиональное (полное дублирование)': 'ППД',
+            'Оригинальная дорожка': 'Оригинальная'
+        }
+        for langdiv in trackersdiv.find_all('',{'class': 'blue_span_float'}):
+            try:
+                langs.append(langs_type[str(langdiv.attrs['title'])])
+            except:
+                langs.append(str(langdiv.attrs['title']))
+
+        newtracker = {'langs': langs}
+        
+        # Get tracker type
+        try:
+            newtracker['tracker'] = trackersdiv.find('div', attrs={'class': 'trackert'}).text.strip()
+        except:
+            newtracker['tracker'] = None
+
+        # Get size
+        try:
+            size_text = trackersdiv.find('div', attrs={'style': 'height:1.2em; overflow: hidden;'}).text.strip()
+            newtracker['size'] = float(size_text[:size_text.find('.')+2])
+        except:
+            newtracker['size'] = 0
+
+        try:
+            newtracker['sid_count'] = int(trackersdiv.find('div', {'title': 'Число раздающих'}).text.strip())
+        except:
+            newtracker['sid_count'] = 0
+
+        newtracker['href'] = top_url + str(trackersdiv.find('a', {'class': 'icon_t download-link'}).attrs['href'])
+
+        newtracker['type'] = type
+
+        trackers.append(newtracker)
+    
+    return trackers
+
+def login_in_rutracker(url):
+    params = {'login_username': username,
+              'login_password': password}
+    print(params)
+    # Login in
+    output = client.post(url, headers=headers, data=params)
+    return output
+
+# Get external (RUTOR, RUTRACKER, KINOZAL) torrent's URL
+def get_ext_torrent(url, tracker_type):
+    if tracker_type == 'rutracer.org':
+        r = login_in_rutracker(url)
+    else:
+        r = client.get(url)
+    bsObj = BeautifulSoup(r.content, 'html.parser') 
+    print(bsObj)
+
+    if tracker_type == 'rutor.info':
+        download_div = bsObj.find('div', {'id': 'download'}).a
+        torrent_url = download_div.find_next_sibling('a').attrs['href']
+        return torrent_url
+    elif tracker_type == 'rutracer.org':
+        download_div = bsObj.find('a', {'id': 'logged-in-username'})
+        #download_div = bsObj.find('a', {'class': 'dl-stub'})
+        print(download_div)
+
+
+# Filter trackers' urls by size or by lang's quality
+def filter_trackers(trackers):
+    filtred_trackers = []
+    if trackers:
+        for item in trackers:
+            remove_flag = 0
+            if (item['size'] < 2) or (item['size'] > 10):
+                remove_flag = 1
+
+            # not Оригинальная - закадровый многоголосый и 3d !!!
+            if not (('ППД' in item['langs']) or ('Оригинальная' in item['langs'])):
+                remove_flag = 1
+
+            # filter unknown trackers
+            # if not (('rutor.info' in item['tracker']) or ('kinozal.tv' in item['tracker'] or ('rutracker.org' in item['tracker']))):
+            if not ('rutracker.org' in item['tracker']):
+                remove_flag = 1
+
+            if not remove_flag: 
+                filtred_trackers.append(item)
+    return filtred_trackers
+
+# Ranging and get best torrent's url
+def get_best_torrent_url(film):
+    trackers = get_trackers(film, 'hd')
+    trackers = filter_trackers(trackers)
+    if trackers: 
+        trackers.sort(key=lambda x: x['sid_count'], reverse = True)
+
+    rip_trackers = get_trackers(film, 'rip')
+    rip_trackers = filter_trackers(rip_trackers)
+    if rip_trackers: 
+        rip_trackers.sort(key=lambda x: x['sid_count'], reverse = True)
+
+    trackers += rip_trackers
+    pprint(trackers)
+
+    #Get torrent's urls cinemate href by
+
+    for item in trackers:
+        r = client.get(item['href'])
+        bsObj = BeautifulSoup(r.content, 'html.parser') 
+        trackers_url = bsObj.find('a', attrs={'rel': 'nofollow'}).attrs['href']
+        if trackers_url:
+            print(trackers_url)
+            torrent_url = get_ext_torrent(trackers_url, item['tracker'])
+            return torrent_url
+    
+    # NEXT - get .torr link on rutor, rutacker, kinozal, etc.
+
+    # Can't get url
+    return None
 
 # Main
 client = requests.session()
@@ -208,9 +341,10 @@ for item in newfilms:
 films = db.films
 
 newfilms = filter_films(newfilms, films)
-
 pprint(newfilms)
 
+torrent_url = get_best_torrent_url(newfilms[2])
+print(torrent_url)
 
 # get torrent_urls
 # if bad torrent - get other url
